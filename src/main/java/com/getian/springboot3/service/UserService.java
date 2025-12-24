2754f21308d57dc221927fdbe5c452b813f70637
etge <etge@iflytek.com>
2025-12-24 10:24:18 +0800

    feat(springboot3):Junit测试使用

---
+++ src/main/java/com/getian/springboot3/service/UserService.java
@@ -0,0 +1,10 @@
+package com.getian.springboot3.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void save(){
+        System.out.println("新增用户。。。");
+    }
+}
--- src/test/java/com/getian/springboot3/Springboot3ApplicationTests.java
+++ src/test/java/com/getian/springboot3/Springboot3ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.getian.springboot3;
 
+import com.getian.springboot3.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot3ApplicationTests {
+    @Autowired
+    private UserService userService;
 
     @Test
     void contextLoads() {
+        userService.save();
     }
 
 }
